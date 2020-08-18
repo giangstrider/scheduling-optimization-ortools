3709b8c526cb9c9cf859a8e73487a82aa84e99be
Hoang Giang Trinh <strider.giang@live.com>
2020-08-18 10:36:34 +1000

    Model JOBS to Start/End IntVar and Optional Bool Var

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ for b in blocked_times:
 ### CONSTRUCT LOCATIONS OBJECTS ###
 locations_dict = {}
 for l in locations:
-    locations_dict[l['factory_customer_id']] = l['employee_id']
+    locations_dict[l['employee_id']] = l['employee_id']
 
 ### CONSTRUCT DISTANCES OBJECTS ###
 total_distance_between_matrix = 0
@@ -48,3 +48,33 @@ for distance_obj in distances:
     tuple_distance = (distance_obj["measure_point"], distance_obj["reference_point"])
     distances_dict[tuple_distance] = distance_obj["hours"]
     total_distance_between_matrix += distance_obj["hours"]
+
+
+### STARTING MODEL THE SOLUTION ###
+# INIT MODEL AND ASSIGN NAMING VARIABLES
+model = cp_model.CpModel()
+
+# Model JOB_TYPE with optional BOOL_VAR
+job_type = collections.namedtuple('booking_type', 'start end interval duration bool_var')
+block_type = collections.namedtuple('booking_type', 'start end interval duration')
+dummy_type = collections.namedtuple('dummy_type', 'start end interval duration')
+all_bookings = {} 
+
+# Model JOBS
+for j in jobs:
+    for e in employees:
+        if j['job_type'] in e['skills'] or 'General' in e['skills']:
+            label_tuple = (j['job_id'], e['employee_id'])
+            start_var = model.NewIntVar(0, horizon, 'start_assign_%s_%s' % label_tuple)
+            duration = j['job_duration']
+            end_var = model.NewIntVar(0, horizon, 'end_assign_%s_%s' % label_tuple)
+            bool_var = model.NewBoolVar('bool_assign_%s_%s' % label_tuple)
+            optional_interval_var = model.NewOptionalIntervalVar(
+                start_var, duration, end_var, bool_var,
+                'optional_interval_assign_%s_%s' % label_tuple
+            )
+
+            all_bookings[label_tuple] = job_type(
+                start=start_var, end=end_var, interval=optional_interval_var,
+                duration=duration, bool_var=bool_var
+            )
