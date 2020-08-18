97b65f40b7d8ddfa4438971cf102bf6cd54c6a8f
Hoang Giang Trinh <strider.giang@live.com>
2020-08-18 10:20:12 +1000

    Add helper for transform variables to solver

---
+++ helper.py
@@ -0,0 +1,28 @@
+import math
+import datetime
+
+def integer_to_day_hour(num_integer, within_day=True, num_hours_per_day=24, start_hour_of_day=6):
+    """Convert integer return from solver to readable date for logging or visualization."""
+    plus_number = num_integer % num_hours_per_day
+    hours = start_hour_of_day + plus_number
+    day = math.floor(num_integer / num_hours_per_day)
+    if plus_number == 0 and day != 0 and not within_day:
+        day = day - 1
+        hours = start_hour_of_day + num_hours_per_day
+
+    date = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"][day]
+    string_return = date + "-" + str(hours)
+    return string_return
+
+def get_weekday_from_datetime(dt):
+    year, month, day = (int(x) for x in dt.split('-'))
+    weekday = datetime.date(year, month, day).weekday()
+    return weekday
+
+def get_distance_between_point(distances_dict, measure_point, reference_point):
+    """Returns the distance between tasks of job measure_point and tasks of job reference_point."""
+    key_tuple = (measure_point, reference_point)
+    if key_tuple not in distances_dict.keys():
+        key_tuple = (reference_point, measure_point)
+    hours = distances_dict[key_tuple]
+    return hours
