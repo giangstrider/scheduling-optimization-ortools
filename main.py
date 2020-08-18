1ae02b61900b68ebc9328233fe05488bba9a108e
Hoang Giang Trinh <strider.giang@live.com>
2020-08-18 10:27:52 +1000

    Convert readable data to integer to passing to solver

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import datetime
 import json
 import math
 
+from config import horizon
+from helper import get_weekday_from_datetime, get_distance_between_point
+
 with open('sample.json') as f:
     data = json.load(f)
 
@@ -18,3 +21,18 @@ print("ASSIGNMENTS ", len(jobs))
 print("BLOCKED_TIMES ", len(blocked_times))
 print("INSPECTORS ", len(employees))
 print("HORIZON ", horizon)
+
+
+### TRANSFORM DATE OF JOB & BLOCKING_TIMES TO INTEGER ###
+for a in jobs:
+    a['origin_expected_date'] = a['expected_date']
+    a['expected_date'] = get_weekday_from_datetime(a['expected_date'])
+    if not a['shipment_date']:
+        a['shipment_date'] = -1
+    else:
+        a['shipment_date'] = get_weekday_from_datetime(a['shipment_date'])
+
+for b in blocked_times:
+    b['origin_requested_date'] = b['requested_date']
+    b['requested_date'] = get_weekday_from_datetime(b['requested_date'])       
+
